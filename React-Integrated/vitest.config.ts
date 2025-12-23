0d1a132d0c0ae18dbe332f176017d544c9fda8fb
Igor Fuchs Pereira <igorfuchs111@gmail.com>
2025-12-23 12:19:58 -0300

    feat: Vitest Alias

--- React-Integrated/vitest.config.ts
+++ React-Integrated/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
@@ -9,5 +10,18 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./tests/unit/vitest.setup.ts",
     exclude: ["**/node_modules/**", "**/tests/e2e/**"],
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layout': path.resolve(__dirname, 'src/components/layout'),
+      '@sections': path.resolve(__dirname, 'src/components/sections'),
+      '@simulation': path.resolve(__dirname, 'src/components/simulation'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
 });
